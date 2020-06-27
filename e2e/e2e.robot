abe130cdc11d1740b1211f93de043d9636915258
natalya-revtova <revtovagn@gmail.com>
2020-06-27 11:31:06 +0500

    Test get svc

--- e2e/e2e.robot
+++ e2e/e2e.robot
@@ -1,6 +1,6 @@
 *** Settings ***
 Library             RequestsLibrary
-Library             e2e.helm.Helm     WITH NAME   helm
+Library             e2e.setup.Setup     WITH NAME   helm
 Test Setup          Install chart
 Test Teardown       Uninstall chart
 
@@ -13,7 +13,7 @@ ${nodeIP}
 Check connection to NodePort
     ${nodeIP}           helm.get_node_ip
     ${nodePort}         helm.get_node_port    ${chartId}
-    Create Session      connection            ${nodeIP}:${nodePort}
+    Create Session      connection            http://${nodeIP}:${nodePort}
     ${resp}             Get request           connection         /
     Should be equal     ${resp.status_code}   ${200}
     #Log     ${chartId}
--- e2e/setup.py
+++ e2e/setup.py
@@ -1,21 +1,23 @@
 import os
 import time
 
-class Helm(object):
+class Setup(object):
     def install_helm_chart(self, name: str):
         os.system("helm install --namespace test " + name + " ./ppw-chart --set fullnameOverride=" + name)
-        time.sleep(10)
+        os.system("kubectl get --namespace test services")
 
     def get_node_port(self, name: str):
         command = "kubectl get --namespace test -o jsonpath={.spec.ports[0].nodePort} services " + name
         stream = os.popen(command)
         nodePort = stream.read()
+        print(nodePort)
         return nodePort
 
     def get_node_ip(self):
         command = 'kubectl get nodes --namespace test -o jsonpath={.items[0].status.addresses[1].address}'
         stream = os.popen(command)
         nodeIP = stream.read()
+        print(nodeIP)
         return nodeIP
 
     def delete_helm_chart(self, name: str):
