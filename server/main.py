e8fc21242feb886f2b8c538d2719a56363d91043
Georgy Timoshenskiy <timoshenskij.gd@edu.spbstu.ru>
2020-06-27 18:56:28 +0300

    Update main.py

--- server/main.py
+++ server/main.py
@@ -1,20 +1,22 @@
-from flask import Flask, jsonify, request
+"""Flask server"""
+
 import os
-import json
 import pickle
-import eli5
-import yaml
 import random
 import string
+from flask import Flask, jsonify, request
+import eli5
+import yaml
 
 CONFIG_FILE = 'config.yaml'
 app = Flask(__name__)
 model = None
-message_id = {}
-id_expl = {}
+message_id_dict = {}
+id_expl_dict = {}
 
 
 def id_generator(size=10, chars=string.ascii_letters + string.digits):
+    """Generate random id."""
     return ''.join(random.choice(chars) for _ in range(size))
 
 
@@ -45,42 +47,49 @@ def add_message():
     """Return new id."""
     content = request.get_json()
 
-    if content['text'] in message_id:
-        content['id'] = message_id[content['text']]
+    if content['text'] in message_id_dict:
+        content['id'] = message_id_dict[content['text']]
     else:
         new_id = id_generator()
-        message_id[content['text']] = new_id
+        message_id_dict[content['text']] = new_id
         content['id'] = new_id
 
     return content
 
 
-@app.route('/v1/p/<id>', methods=["GET"])
-def get_result(id):
+@app.route('/v1/p/<message_id>', methods=["GET"])
+def get_result(message_id):
     """Create HTML page with ML-model result."""
     html_page = None
 
-    if id in id_expl:
-        html_page = eli5.formatters.html.format_as_html(id_expl[id])
+    if message_id in id_expl_dict:
+        html_page = eli5.formatters.html.format_as_html(id_expl_dict[message_id])
     else:
-        for message, search_id in message_id.items():
-            if search_id == id:
-                id_expl[id] = eli5.sklearn.explain_prediction_linear_regressor(model['cls'], message, model['vec'])
-                html_page = eli5.formatters.html.format_as_html(id_expl[id])
+        for message, search_id in message_id_dict.items():
+            if search_id == message_id:
+                if model is not None:
+                    id_expl_dict[message_id] = eli5.sklearn.explain_prediction_linear_regressor(model['cls'],
+                                                                                                message,
+                                                                                                model['vec'])
+                    html_page = eli5.formatters.html.format_as_html(id_expl_dict[message_id])
                 break
 
     if html_page is not None:
         return html_page
-    else:
-        return 'Id not found', 400
+
+    return 'Id not found', 400
 
 
 def main():
+    """Load config and run flask app."""
+
     global model
+
     with open(CONFIG_FILE) as file:
         config_data = yaml.load(file, Loader=yaml.FullLoader)
 
-    model = pickle.load(open(os.path.join(config_data['common_dir'], config_data['model_name']), 'rb'))
+    if os.path.exists(os.path.join(config_data['common_dir'], config_data['model_name'])):
+        model = pickle.load(open(os.path.join(config_data['common_dir'], config_data['model_name']), 'rb'))
 
     app.run(debug=True, host='0.0.0.0', port=config_data['port'])
 
