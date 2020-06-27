e8640349f8b9ba338ff36167619049d53c7aac36
natalya-revtova <revtovagn@gmail.com>
2020-06-27 22:50:54 +0500

    Test requests

--- e2e/setup.py
+++ e2e/setup.py
@@ -19,7 +19,8 @@ class Setup(object):
         os.system("helm install --namespace test " + name + " ./ppw-chart --set fullnameOverride=" + name)
         nodeIP = self.get_node_ip
         nodePort = self.get_node_port(name)
-        print(nodeIP, nodePort, "\n")
+        os.system("echo" + str(nodeIP))
+        os.system("echo" + str(nodePort))
         #resp = requests.get("https://" + str(self.get_node_ip) + ":" + str(self.get_node_port(name)))
         #print(resp.status_code)
         #print(resp.json())
