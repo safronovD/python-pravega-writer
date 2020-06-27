5318666b11fc5a3bf960876957474750b86992e2
natalya-revtova <revtovagn@gmail.com>
2020-06-27 12:06:55 +0500

    Check nodeIP

--- e2e/setup.py
+++ e2e/setup.py
@@ -9,16 +9,14 @@ class Setup(object):
         return nodePort
 
     def get_node_ip(self):
-        command = 'kubectl get nodes --namespace test -o jsonpath={.items[0].status.addresses[1].address}'
+        command = 'kubectl get nodes --namespace test -o jsonpath={.items[0].status.addresses[0].address}'
         stream = os.popen(command)
         nodeIP = stream.read()
         return nodeIP
 
     def install_helm_chart(self, name: str):
         os.system("helm install --namespace test " + name + " ./ppw-chart --set fullnameOverride=" + name)
-        os.system("kubectl get --namespace test services")
         os.system("kubectl get nodes --namespace test -o jsonpath={.items[0].status.addresses[1].address}")
-        os.system("kubectl get --namespace test -o jsonpath={.spec.ports[0].nodePort} services " + name)
 
     def delete_helm_chart(self, name: str):
         os.system("helm delete --namespace test " + name)
