3bc6ffabc4b01f8a9d22afa1f496342a6299823f
natalya-revtova <revtovagn@gmail.com>
2020-06-29 19:50:00 +0500

    Test curl

--- e2e/setup.py
+++ e2e/setup.py
@@ -17,6 +17,7 @@ class Setup(object):
     def install_helm_chart(self, name: str):
         os.system("helm install --namespace test " + name + " ./ppw-chart --set fullnameOverride=" + name)
         os.system("curl -i http://192.168.70.211:31123")
+        os.system("curl -i http://192.168.70.211:30798")
 
     def delete_helm_chart(self, name: str):
         os.system("helm delete --namespace test " + name)
