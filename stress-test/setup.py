d1c8acb0d44563c546501b0deed40e33ce76088e
natalya-revtova <revtovagn@gmail.com>
2020-07-06 19:39:15 +0500

    test

--- stress-test/setup.py
+++ stress-test/setup.py
@@ -5,15 +5,14 @@ sys.path.append(".")
 from e2e.setup import Setup
 
 if __name__ == "__main__":
-    chartName = sys.argv[1]
-    obj = Setup(chartName)
+    #chartName = sys.argv[1]
+    #obj = Setup(chartName)
 
     #obj.install_helm_chart()
 
-    nodeIP = obj.get_node_ip()
+    #nodeIP = obj.get_node_ip()
     #nodePort = obj.get_node_port()
-    os.system("echo ${nodeIP}")
 
-    #os.system("locust -f ./stress-test/request.py --csv=reports/result --host=http://${nodeIP}:${nodePort} --headless -u 1000 -r 100 --run-time 40s")
+    os.system("locust -f ./stress-test/request.py --csv=reports/result --host=http://192.168.70.211:30841 --headless -u 1000 -r 100 --run-time 40s")
 
     #obj.delete_helm_chart()
