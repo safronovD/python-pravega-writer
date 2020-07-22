43bb0982df9c90dfc3f9c081641e10077a982935
Georgy Timoshenskiy <timoshenskij.gd@edu.spbstu.ru>
2020-07-22 16:22:15 +0300

    Update logger.py

--- src/common/log/logger.py
+++ src/common/log/logger.py
@@ -5,13 +5,13 @@ import os
 
 
 def init_logger(*args):
-
-    print(os.listdir(path="./src"))
-    print(os.listdir(path="./src/common"))
-    print(os.listdir(path="./src/common/log"))
-    with open('./src/common/log/logger_config.yaml', 'r') as f:
-        log_cfg = yaml.safe_load(f.read())
+    if args[0] == 'ci':
+        with open('./src/common/log/logger_config.yaml', 'r') as f:
+            log_cfg = yaml.safe_load(f.read())
+    elif args[0] == 'app':
+        with open('./log/logger_config.yaml', 'r') as f:
+            log_cfg = yaml.safe_load(f.read())
     logging.config.dictConfig(log_cfg)
-    if args:
-        return logging.getLogger(args[0])
-    return logging.getLogger()
+
+    return logging.getLogger(args[0])
+
