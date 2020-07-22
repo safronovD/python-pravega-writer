e5b6b3fc0dc16652bd018464ad1f16673aa49129
Georgy Timoshenskiy <timoshenskij.gd@edu.spbstu.ru>
2020-07-22 13:44:56 +0300

    Update logger.py

--- src/common/log/logger.py
+++ src/common/log/logger.py
@@ -7,7 +7,7 @@ import os
 def init_logger(*args):
 
     print(os.listdir(path="."))
-    with open('./log/logger_config.yaml', 'r') as f:
+    with open('./src/common/log/logger_config.yaml', 'r') as f:
         log_cfg = yaml.safe_load(f.read())
     logging.config.dictConfig(log_cfg)
     if args:
