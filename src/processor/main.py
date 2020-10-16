1e65a78743ceca8fefaa6418a9e311aca67fbb17
safronovD <dxd360@outlook.com>
2020-10-16 20:02:37 +0300

    add kafka-connector
    - create processor module (process new issues from server using kafka topic)
    - remove processing logic from server
    - remove connector (logic in ml-controller now)
    - update docs
    - update yaml deploying files
    - update README.md

---
+++ src/processor/main.py
@@ -0,0 +1,41 @@
+import os
+from pickle import load as pickle_load
+import eli5
+import yaml
+from json import dumps
+from kafka import KafkaConsumer, KafkaProducer
+
+CONFIG_FILE = 'config.yaml'
+
+
+def main():
+    with open(CONFIG_FILE) as file:
+        config_data = yaml.load(file, Loader=yaml.FullLoader)
+
+    if os.path.exists(os.path.join(config_data['common_dir'], config_data['model_name'])):
+        model = pickle_load(open(os.path.join(config_data['common_dir'], config_data['model_name']), 'rb'))
+
+    consumer = KafkaConsumer(config_data['new_issues_topic'], auto_offset_reset='latest',
+                             bootstrap_servers=[config_data['kafka_server']],
+                             value_deserializer=lambda x:
+                             x.decode('utf-8', 'ignore'))
+
+    producer = KafkaProducer(bootstrap_servers=[config_data['kafka_server']],
+                             value_serializer=lambda x:
+                             dumps(x).encode('utf-8'))
+
+    for msg in consumer:
+        expl = eli5.sklearn.explain_prediction_linear_regressor(model['cls'],
+                                                                msg.value,
+                                                                model['vec'])
+        response = eli5.formatters.as_dict.format_as_dict(expl)
+
+        print(response)
+
+        producer.send(config_data['processed_issues_topic'], key=msg.key, value=response)
+
+    consumer.close()
+
+
+if __name__ == '__main__':
+    main()
